93dea91af3dc7a7aab110b869c78566bc70b34ec
MiddleBenchDev <middlebenchdev@gmail.com>
2024-04-04 16:46:00 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,6 @@ app.use(bodyParser.json())
 app.use(morgan("dev"))
 app.use(cors())
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD")
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-
-//     next()
-// })
-
 app.use(ListRouter)
 app.use(TaskRouter)
 app.use(UserRouter)
